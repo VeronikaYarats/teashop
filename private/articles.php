6b14006ae832e012db2499051e331d756376d0a4
Veronika Kurochkinf <notomorrow1@yandex.ru>
2015-09-17 16:14:13 +0300

    add function article_edit

--- private/articles.php
+++ private/articles.php
@@ -73,4 +73,64 @@ function article_add_new($array_params)
     return(db_insert($table_name, $array_params));
 }
 
+
+
+
+/**
+ * редактирует статью с идентификатором $id
+ * @param $id идентификатор редактируемой записи
+ * @param $array_params - массив с данными для редактироания
+ * @param [page_title] - title страницы
+ * @param [name] - название стать
+ * @param [contents] - содержимое статьи
+ * @param [public] - публикация статьи
+ * @return EINVAL в случае ошибки входных параметров
+ * @return EBASE в случае ошибки связи с базой
+ * @return 0 в случае успешного редактирования
+ */
+function article_edit($id, $array_params)
+{
+    global $table_name;
+    
+    if (!is_numeric($id) || !isset($id)) {
+        dbg_err("Incorrect id"); 
+        return EINVAL;
+    }
+    if(!isset($array_params["page_title"])) { 
+        dbg_err("Not set page title");    
+        return EINVAL;    
+    }
+    
+    if(!isset($array_params["page_title"])) { 
+        dbg_err("Not set page title");    
+        return EINVAL;    
+    }
+    
+    if(!isset($array_params["name"])) {
+        dbg_err("Not set name");    
+        return EINVAL;
+    }
+    
+    if(!isset($array_params["contents"])) {
+        dbg_err("Not set contents");    
+        return EINVAL;    
+    }
+    
+    if(!isset($array_params["public"])) {
+        dbg_err("Not set public");    
+        return EINVAL;    
+    }
+    
+    if (article_get_by_id($id) <= 0) {
+        dbg_err("Article not found"); 
+        return EINVAL;
+    }
+    
+    if(!db_update($table_name, $id, $array_params)) {
+    	dbg_err("Can not edit article");
+    	return EBASE;
+    } else 
+        return 0;
+}
+
 ?>
