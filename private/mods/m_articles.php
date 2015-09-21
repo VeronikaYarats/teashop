2aac1e126ba3ad45a769999c745961b28ad39d36
Veronika Kurochkinf <notomorrow1@yandex.ru>
2015-09-21 22:21:02 +0300

    get article by key if not set article id

--- private/mods/m_articles.php
+++ private/mods/m_articles.php
@@ -4,20 +4,18 @@
 function m_articles($argv = array())
 {
     $tpl = new strontium_tpl("private/tpl/m_articles.html", array(), false);
-    if(isset($_GET['id']))
-	   $id = $_GET['id'];
-	else 
-	   $id = 80; /* Id статьи по умолчанию, если она не задана через url */
-
-    $article = article_get_by_id($id);
-    switch($article) {
-    	case EINVAL:
-    		$tpl->assign("article_error_message");
-    	    break;
-
-    	default:
-    		$tpl->assign("article", $article);
-    }
+    
+    if(isset($argv['id']))
+        $article = article_get_by_id($argv['id']);
+    else 
+        if(isset($argv['key']))
+            $article = article_get_by_key($argv['key']);
+        else 
+            $article = article_get_by_key("welcome");
+    if($article < 0)
+            $tpl->assign("article_error_message");
+    else
+    $tpl->assign("article", $article);
     page_set_title($article['page_title']);
     return $tpl->result();
 }
