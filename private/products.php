d7dd7d6e003cb81383f43dc6b0eb07324d2b841f
Veronika Kurochkinf <notomorrow1@yandex.ru>
2015-09-22 15:32:15 +0300

    add function get_product_by_category

--- private/products.php
+++ private/products.php
@@ -1,5 +1,15 @@
 <?php
+/* Функции для работа с продуктами */
 
-// тут функции для работы с продуктами
+/**
+ * Возаращает название, страну, вес, цену продукта по категории
+ * @param $id - категории продукта
+ */
+function get_product_by_category($id)
+{
+	$query = "SELECT name_product, country, weight, price FROM products 
+	WHERE product_category_id = ". $id;
+	return (db_query($query));
+}
 
 ?>
