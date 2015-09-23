3b3a813d8fe30cdeb97abd1ecde2746d8b3e4675
Veronika Kurochkinf <notomorrow1@yandex.ru>
2015-09-23 16:43:28 +0300

    add dinamic properties to template

--- private/mods/m_products.php
+++ private/mods/m_products.php
@@ -1,21 +1,30 @@
 <?php 
-/*  код обслуживающий продукты */
+/*  РєРѕРґ РѕР±СЃР»СѓР¶РёРІР°СЋС‰РёР№ РїСЂРѕРґСѓРєС‚С‹ */
 
 function m_products($argv=array())
 {
-	$tpl = new strontium_tpl("private/tpl/m_products.html", array(), false);
-	if(isset($argv['cat_id']))
-	   $id = $argv['cat_id'];
-	
-	$products = get_product_by_category($id);
-	
-	if($products < 0) //если запрос не нашел продукты
-	   $tpl->assign("product_error_message");
-	else {
-	foreach($products as $product)
-	   $tpl->assign("products", $product);
-	}
-	return $tpl->result();
+    $tpl = new strontium_tpl("private/tpl/m_products.html", array(), false);
+    if(isset($argv['cat_id']))
+       $cat_id = $argv['cat_id'];
+    
+    $products = products_get_list_by_category($cat_id);
+    if($products < 0) //РµСЃР»Рё Р·Р°РїСЂРѕСЃ РЅРµ РЅР°С€РµР» РїСЂРѕРґСѓРєС‚С‹
+       $tpl->assign("product_error_message");
+    else {
+    foreach($products as $product) {
+       $tpl->assign("products", $product);
+       $product_id = $product['id'];
+       $properties = product_get_dynamic_properties($product_id);
+       if($properties < 0)
+           continue;
+       else 
+	       foreach ($properties as $property_name => $property_value)
+		       $tpl->assign('dymnamic_property', 
+		                    array('name' => $property_name, 'value' => $property_value));
+		       
+    }   
+    }
+    return $tpl->result();
 }
 
 ?>
