f2ec71b0ee53b37f1cb0aafe465bc430fcade79b
Veronika Kurochkinf <notomorrow1@yandex.ru>
2015-09-24 20:09:05 +0300

    getting category properties in product_get_dynamic_properties() put into function product_category_get_dynamic_properties()

--- private/products.php
+++ private/products.php
@@ -29,9 +29,26 @@ function products_get_list_by_category($cat_id)
 }
 
 /**
+ * Получает список динамических свойст категории
+ * @param $cat_id - id категории продукта
+ * @return массив в формате id свойства => array('название свойства', 'тип свойства')
+ */
+function product_category_get_dynamic_properties($cat_id)
+{
+    $cat_id = (int)$cat_id;
+	$query = "SELECT id, name, type FROM product_properties 
+              WHERE product_category_id = " . $cat_id;
+    $category_properties = db_query($query);
+    foreach($category_properties as $category_property) 
+        $properties[$category_property['id']] = array('name' => $category_property['name'],
+                                                      'type' => $category_property['type']);
+    return $properties;
+}
+
+/**
  * Получает список динамических свойств продукта
  * @param $product_id - id продукта
- * @return массив в формате ('название свойства' => 'значение свойства')
+ * @return массив в формате id свойства => array('название свойства', 'значение свойства')
  *         или ошибка
  */
 function product_get_dynamic_properties($product_id)
@@ -43,36 +60,28 @@ function product_get_dynamic_properties($product_id)
         return $rows;
          
     $cat_id = $rows[0]['product_category_id'];
-    
-    /* получение динамических свойств для продукта $product_id */
-    $query = "SELECT id, name, type FROM product_properties 
-              WHERE product_category_id = " . $cat_id;
-    $product_properties = db_query($query); 
+    $product_properties = product_category_get_dynamic_properties($cat_id);
     if ($product_properties < 0)
         return $product_properties;
-
-    $properties = array();
-    foreach($product_properties as $product_property) { 
-
+        
+    foreach($product_properties as $id => $product_property)
         /* анализ типа данных свойства */
         switch ($product_property['type']) { 
         /* тип данных ENUM */
         case 'enum':
             /* получение значение свойства */
             $query = "SELECT variant FROM product_property_enum " .
-                     "WHERE property_id = " . $product_property['id'] . " " .
+                     "WHERE property_id = " . $id . " " .
                      "AND id = (" .
                           "SELECT value FROM product_properties_values " .
                           "WHERE product_id = ". $product_id . " " .
-                          "AND property_id = " . $product_property['id'] . 
+                          "AND property_id = " . $id . 
                                ')';
-
             $rows = db_query($query);
             if ($rows < 0)
               return $rows;
             $value = $rows[0]['variant'];
-
-            $properties[$product_property['id']] = array("name" => $product_property['name'],
+            $properties[$id] = array("name" => $product_property['name'],
                                                          "value" => $value);
             break;
             
@@ -80,21 +89,19 @@ function product_get_dynamic_properties($product_id)
         case 'string':
         /* тип данных INTEGER */
         case 'integer':
-        	/* получение значение свойства */
-        	$query = "SELECT value FROM product_properties_values " .
+            /* получение значение свойства */
+            $query = "SELECT value FROM product_properties_values " .
                           "WHERE product_id = ". $product_id . " " .
-                          "AND property_id = " . $product_property['id'];
-        	$rows = db_query($query);
-        	if ($rows < 0)
+                          "AND property_id = " . $id;
+            $rows = db_query($query);
+            if ($rows < 0)
               return $rows;
             $value = $rows[0]['value'];
-        
             $properties[$product_property['id']] = array("name" => $product_property['name'],
                                                          "value" => $value);
-        	break;
+            break;
         }
-    }
-    return $properties;
+  return $properties;
 }
 
 
