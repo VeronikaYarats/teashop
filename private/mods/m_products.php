3ada6bd441a53bed42fc5fe058efacfa925847a4
Veronika Kurochkinf <notomorrow1@yandex.ru>
2017-02-16 12:31:53 +0300

    added support product image

--- private/mods/m_products.php
+++ private/mods/m_products.php
@@ -3,15 +3,17 @@
 
 function m_products($argv=array())
 {
-    $tpl = new strontium_tpl("private/tpl/m_products.html", array(), false);
+	$products_count = 0;
+    $tpl = new strontium_tpl("private/tpl/m_products.html", $global_marks, false);
+    $tpl->assign(product_search);
     if(isset($argv['cat_id']))
        $cat_id = $argv['cat_id'];
     
     $products = products_get_list_by_category($cat_id);
-    if($products < 0) //если запрос не нашел продукты
-       $tpl->assign("product_error_message");
-    else {
     foreach($products as $product) {
+       if (!$product['public'])
+          continue;  
+		
        $tpl->assign("products", $product);
        $product_id = $product['id'];
        $properties = product_get_dynamic_properties($product_id);
@@ -20,10 +22,17 @@ function m_products($argv=array())
        else 
 	       foreach ($properties as $property)
 		       $tpl->assign('dymnamic_property',$property);
-		       
-    }   
+		$products_count++;   
+		$image_sizes = array('big' => array('w' => 0), 
+							'mini' => array('w' => 150));
+		$images = get_first_object_image('products', $product_id, $image_sizes);
+		$tpl->assign('image',$images);
+	    
     }
+    if($products_count < 1)
+    	$tpl->assign("product_error_message"); 	
     return $tpl->result();
+    
 }
 
 ?>
