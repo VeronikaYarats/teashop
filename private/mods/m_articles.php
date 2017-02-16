73e5bf766c7486095d3c1a87ccc10944d0b6de83
Veronika Kurochkinf <notomorrow1@yandex.ru>
2017-02-16 12:02:05 +0300

    fixed article public

--- private/mods/m_articles.php
+++ private/mods/m_articles.php
@@ -3,19 +3,20 @@
 
 function m_articles($argv = array())
 {
-    $tpl = new strontium_tpl("private/tpl/m_articles.html", array(), false);
+   $tpl = new strontium_tpl("private/tpl/m_articles.html", array(), false);
     
     if(isset($argv['id']))
         $article = article_get_by_id($argv['id']);
     else 
-        if(isset($argv['key']))
+        if(isset($argv['key'])) 
             $article = article_get_by_key($argv['key']);
         else 
             $article = article_get_by_key("welcome");
-    if($article < 0)
+            
+    if( $article < 0 || $article['public'] == 0)
             $tpl->assign("article_error_message");
-    else
-    $tpl->assign("article", $article);
+    else 
+        $tpl->assign("article", $article);
     page_set_title($article['page_title']);
     return $tpl->result();
 }
